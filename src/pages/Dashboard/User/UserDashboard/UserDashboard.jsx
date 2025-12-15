bab0bd0ad7300048279e32469ec1b0c09fbbb488
NusratJahanNila <nnusratjahan330@gmail.com>
2025-12-16 01:32:48 +0600

    user dashboard

--- src/pages/Dashboard/User/UserDashboard/UserDashboard.jsx
+++ src/pages/Dashboard/User/UserDashboard/UserDashboard.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import DashboardBanner from './DashboardBanner';
+import DashboardStats from './DashboardStats';
+import RecentLessons from './RecentLessons';
+import QuickShortcuts from './QuickShortcuts';
+import DashboardAnalytics from './DashboardAnalytics';
 
 const UserDashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="p-6 space-y-10">
+
+      <DashboardBanner />
+
+      <DashboardStats />
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <RecentLessons />
+        <QuickShortcuts />
+      </div>
+
+      <DashboardAnalytics />
+
+    </div>
+  );
 };
 
 export default UserDashboard;
